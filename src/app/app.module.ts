359a3b77b0da09f89b62b68cde5c4df30cea2246
Marco Andres Silva <marco.silvaponce10@gmail.com>
2025-07-29 23:01:27 -0400

    feat: cofig app-module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,14 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 @NgModule({
-  declarations: [
+  declarations: [ // Si creas un nuevo componente, ¡debes declararlo aquí para que Angular lo reconozca!
     AppComponent // Aquí se declaran TODOS los componentes, directivas y pipes de este módulo.
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,  // Aquí se importan OTROS MÓDULOS que necesitemos.
   ],
-  providers: [], // Aquí se declaran los SERVICIOS que necesitemos.
+  providers: [], // Es el lugar para registrar servicios que estarán disponibles para toda la aplicación.
   bootstrap: [AppComponent] // Aquí se indica con cual componente se inicia en la APP.
 })
 export class AppModule { }
