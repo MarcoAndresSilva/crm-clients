6060ccdb20b203a947dba9bcfe4e57ec51437b5f
Marco Andres Silva <marco.silvaponce10@gmail.com>
2025-07-30 00:41:37 -0400

    feat: add html to header component stiles and logic from layout

--- src/app/layout/components/header/header.component.ts
+++ src/app/layout/components/header/header.component.ts
@@ -1,11 +1,11 @@
-import { Component } from '@angular/core';
-
+import { Component, Input } from '@angular/core';
+import { MatSidenav } from '@angular/material/sidenav';
 @Component({
   selector: 'app-header',
   standalone: false,
   templateUrl: './header.component.html',
-  styleUrl: './header.component.scss'
+  styleUrls: ['./header.component.scss']
 })
 export class HeaderComponent {
-
+  @Input() sidenav!: MatSidenav; // Recibimos la referencia del sidenav y el ! indica que confiamos en qeu este valor sera proporcionado por el padre layout.component
 }
