9bee66bb5b38cdf2b0b6d593168b55edb13604bb
Marco Andres Silva <marco.silvaponce10@gmail.com>
2025-08-01 00:05:01 -0400

    feat: add logic to show data in table example

--- src/app/features/clients/pages/client-list/client-list.component.ts
+++ src/app/features/clients/pages/client-list/client-list.component.ts
@@ -1,11 +1,22 @@
 import { Component } from '@angular/core';
 
+// Datos de ejemplo
+const ELEMENT_DATA = [
+  {id: 1, name: 'Juan Pérez', company: 'Tech Solutions', email: 'juan.perez@tech.com'},
+  {id: 2, name: 'Ana Gómez', company: 'Innovate Corp', email: 'ana.gomez@innovate.com'},
+  {id: 3, name: 'Carlos Sánchez', company: 'Marketing Digital', email: 'carlos@marketing.cl'},
+  {id: 4, name: 'Laura Fernández', company: 'Consultores Asociados', email: 'laura.f@consultores.com'},
+];
+
 @Component({
   selector: 'app-client-list',
   standalone: false,
   templateUrl: './client-list.component.html',
-  styleUrl: './client-list.component.scss'
+  styleUrls: ['./client-list.component.scss']
 })
 export class ClientListComponent {
+
+  displayedColumns: string[] = ['id', 'name', 'company', 'email'];
+  dataSource = ELEMENT_DATA;
 
 }
