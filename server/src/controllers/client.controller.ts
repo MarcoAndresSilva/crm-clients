cda357ba787f1566747cb0e409bad84c420c460b
Marco Andres Silva <marco.silvaponce10@gmail.com>
2025-08-02 22:49:14 -0400

    feat: add first endpoint add folder controllers and add file client.controller.tsa

---
+++ server/src/controllers/client.controller.ts
@@ -0,0 +1,18 @@
+import type { Request, Response } from 'express';
+import ClientModel from '../models/Client-model';
+
+export const getClients = async (req: Request, res: Response) => {
+    try {
+        const clients = await ClientModel.find();
+        res.status(200).json(clients);
+    } catch (error) {
+        const errorMessage = error instanceof Error ? error.message : 'Error desconocido';
+        res.status(500).json({ message: 'Error al obtener los clientes', error: errorMessage });
+    }
+};  
+
+// TODO // --- Aquí añadiremos las otras funciones del CRUD ---
+// export const createClient = async ...
+// export const getClientById = async ...
+// export const updateClient = async ...
+// export const deleteClient = async ...
