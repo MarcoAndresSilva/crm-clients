70120bdacf99af1e489e68e3f37fdf911c83d74d
Marco Andres Silva <marco.silvaponce10@gmail.com>
2025-08-02 23:09:27 -0400

    feat: connect routes to main server in index.ts add clientRoutes

--- server/src/index.ts
+++ server/src/index.ts
@@ -2,6 +2,8 @@ import express, { type Express, type Request, type Response } from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
 import connectDB from './config/database';
+import clientRoutes from './routes/client.routes';
+
 
 // Carga las variables de entorno del fichero .env
 dotenv.config();
@@ -15,8 +17,12 @@ const port = process.env.PORT || 3000;
 app.use(cors()); // --- Middlewares --- Habilita CORS para permitir peticiones desde el frontend de Angular
 app.use(express.json()); // Permite al servidor entender JSON en el cuerpo de las peticiones (para POST, PUT, etc.)
 
+// Le decimos a la app que todas las rutas definidas en 'clientRoutes'
+// estarán bajo el prefijo '/api/clients'.
+app.use('/api/clients', clientRoutes);
+
 app.get('/', (req: Request, res: Response) => { // --- Ruta de Prueba ---
-    res.send('¡Hola desde el servidor Express con TypeScript!');
+    res.send('¡API del CRM funcionando correctamente!');
 });
 
 app.listen(port, () => { // --- Iniciar el Servidor ---
