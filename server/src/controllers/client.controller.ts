53089d765e4135d12a1665f09364fd5cc18abc52
Marco Andres Silva <marco.silvaponce10@gmail.com>
2025-08-04 13:21:05 -0400

    feat: add controllers to complete the CRUD

--- server/src/controllers/client.controller.ts
+++ server/src/controllers/client.controller.ts
@@ -1,9 +1,7 @@
 import { type Request, type Response } from 'express';
 import ClientModel from '../models/Client-model';
 
-// @desc    Obtener todos los clientes
-// @route   GET /api/clients
-// @access  Public
+// @desc    Obtener todos los clientes - @route   GET /api/clients - @access  Public
 export const getClients = async (req: Request, res: Response) => { 
     try {
         const clients = await ClientModel.find();
@@ -14,9 +12,7 @@ export const getClients = async (req: Request, res: Response) => {
     }
 };  
 
-// @desc    Crear un nuevo cliente
-// @route   POST /api/clients
-// @access  Public
+// @desc    Crear un nuevo cliente - @route   POST /api/clients - @access  Public
 export const createClient = async (req: Request, res: Response) => {
     try {
             const {name, company, email, phone} = req.body; // extrae los datos del cuerpo de la solicitud
@@ -33,9 +29,58 @@ export const createClient = async (req: Request, res: Response) => {
             const errorMessage = error instanceof Error ? error.message : 'Error desconocido';
             res.status(500).json({ message: 'Error al crear el cliente', error: errorMessage });
     }
-}
+};
 
-// TODO // --- Aquí añadiremos las otras funciones del CRUD ---
-// export const getClientById = async ...
-// export const updateClient = async ...
-// export const deleteClient = async ...
+// @desc    Obtener un cliente por su ID - @route   GET /api/clients/:id - @access  Public
+export const getClientById = async (req: Request, res: Response) => {
+    try {
+        const client = await ClientModel.findById(req.params.id);
+        if (!client) {
+            return res.status(404).json({ message: 'Cliente no encontrado' });
+        }
+        res.status(200).json(client);
+    } catch (error) {
+        const errorMessage = error instanceof Error ? error.message : 'Error desconocido';
+        res.status(500).json({ message: 'Error al obtener el cliente', error: errorMessage });
+    }
+};
+
+// @desc    Actualizar un cliente por su ID - @route   PUT /api/clients/:id - @access  Public
+export const updateClient = async (req: Request, res: Response) => {
+    try {
+        const { name, company, email, phone } = req.body;
+        const clientId = req.params.id;
+
+        const updatedClient = await ClientModel.findByIdAndUpdate(  // Buscamos el cliente y lo actualizamos en un solo paso { new: true } hace que Mongoose devuelva el documento actualizado, no el original
+            clientId, 
+            { name, company, email, phone },
+            { new: true, runValidators: true }
+        );
+
+        if (!updatedClient) {
+            return res.status(404).json({ message: 'Cliente no encontrado para actualizar' });
+        }
+
+        res.status(200).json(updatedClient);
+    } catch (error) {
+        if(error instanceof Error && 'code' in error && (error as any).code === 11000) {
+            return res.status(409).json({ message: 'El correo electrónico ya esta registrado.' });
+        }
+        const errorMessage = error instanceof Error ? error.message : 'Error desconocido';
+        res.status(500).json({ message: 'Error al actualizar el cliente', error: errorMessage });
+    }
+};
+
+// @desc    Eliminar un cliente por su ID - @route   DELETE /api/clients/:id - @access  Public
+export const deleteClient = async (req: Request, res: Response) => {
+    try {
+        const deleteClient = await ClientModel.findByIdAndDelete(req.params.id);
+        if (!deleteClient) {
+            return res.status(404).json({ message: 'Cliente no encontrado para eliminar' });
+        }
+        res.status(200).json({ message: 'Cliente eliminado correctamente' });
+    } catch (error) {
+        const errorMessage = error instanceof Error ? error.message : 'Error desconocido';
+        res.status(500).json({ message: 'Error al eliminar el cliente', error: errorMessage });
+    }
+}; 
