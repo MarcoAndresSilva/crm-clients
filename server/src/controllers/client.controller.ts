77414bb438a1b3fa504735b2af451e181f39e536
Marco Andres Silva <marco.silvaponce10@gmail.com>
2025-08-03 22:02:55 -0400

    feat: add controller createClient and add roue in client.route

--- server/src/controllers/client.controller.ts
+++ server/src/controllers/client.controller.ts
@@ -14,8 +14,28 @@ export const getClients = async (req: Request, res: Response) => {
     }
 };  
 
+// @desc    Crear un nuevo cliente
+// @route   POST /api/clients
+// @access  Public
+export const createClient = async (req: Request, res: Response) => {
+    try {
+            const {name, company, email, phone} = req.body; // extrae los datos del cuerpo de la solicitud
+            if(!name || !company || !email || !phone) { 
+                return res.status(400).json({ message: 'Todos los campos son obligatorios' });
+            }
+            const newClient = new ClientModel({name, company, email, phone}); // crea una nueva instancia del modelo cliente
+            const savedClient = await newClient.save(); // guarda el nuevo cliente en la base de datos
+            res.status(201).json(savedClient);
+    } catch (error) {
+        if (error instanceof Error && 'code' in error && (error as any).code === 11000) {
+            return res.status(409).json({ message: 'El correo electrónico ya está registrado.' }); // 409 Conflict
+            }
+            const errorMessage = error instanceof Error ? error.message : 'Error desconocido';
+            res.status(500).json({ message: 'Error al crear el cliente', error: errorMessage });
+    }
+}
+
 // TODO // --- Aquí añadiremos las otras funciones del CRUD ---
-// export const createClient = async ...
 // export const getClientById = async ...
 // export const updateClient = async ...
 // export const deleteClient = async ...
--- server/src/routes/client.routes.ts
+++ server/src/routes/client.routes.ts
@@ -1,14 +1,13 @@
 // src/routes/client.routes.ts
 import { Router } from 'express';
-import { getClients } from '../controllers/client.controller';
+import { getClients, createClient} from '../controllers/client.controller';
 
 const router = Router();
 
 // Definimos la ruta y le asignamos el controlador
 router.get('/', getClients);
-
+router.post('/', createClient);
 // --- Aquí añadiremos las otras rutas del CRUD ---
-// router.post('/', createClient);
 // router.get('/:id', getClientById);
 // router.put('/:id', updateClient);
 // router.delete('/:id', deleteClient);
