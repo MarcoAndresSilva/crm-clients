994b02125cc030701d1410f8f216d9a9775ed6b7
Marco Andres Silva <marco.silvaponce10@gmail.com>
2025-08-05 22:01:41 -0400

    feat: define _id? in client.model to consist with the backend

--- src/app/core/models/client.model.ts
+++ src/app/core/models/client.model.ts
@@ -1,8 +1,9 @@
 export interface Client {
-    id?: string; // el id sera opcional al crear pero sera requerido al editar, lo generara mongoDB 
+    _id?: string; // el id sera opcional al crear pero sera requerido al editar, lo generara mongoDB 
     name: string;
     company: string;
     email: string;
     phone?: string; // opcional
     createdAt?: Date; // la fecha de creacion sera opcional y la generara mongoDB
+    updatedAt?: Date; // la fecha de actualizacion sera opcional y la generara mongoDB
 }
