00786c69264b7c293cc675e6eecaf3db8d25bded
Marco Andres Silva <marco.silvaponce10@gmail.com>
2025-08-10 22:21:42 -0400

    refactor(service): implement reactive state with Subject
    Refactors `ClientService` to include a reactive notification system for data changes.
    
    - Introduces a private `Subject` (`_clientsUpdated$`) to act as a communication channel.
    - Exposes a public observable (`clientsUpdated$`) for components to subscribe to.
    - Adds a public method `notifyClientsUpdated()` to allow other parts of the application to trigger an update notification.
    - This change decouples components from each other, enabling a more reactive and maintainable state management pattern.

--- src/app/core/services/client.service.ts
+++ src/app/core/services/client.service.ts
@@ -1,7 +1,6 @@
-import { Handler } from './../../../../server/node_modules/arg/index.d';
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Observable, catchError, throwError, delay } from 'rxjs';
+import { Observable, catchError, throwError, Subject } from 'rxjs';
 import { Client, PaginatedClientsResponse } from '../models/client.model';
 import { environment } from '../../../environments/environment';
 
@@ -12,7 +11,14 @@ export class ClientService {
 
   private apiUrl = `${environment.apiUrl}/clients`; // defino la url base de la API de clientes
 
+  private _clientsUpdated$ = new Subject<void>(); // crea un pulso o señal para notificar clientes actualizados
+
+
 constructor( private http: HttpClient) { } // inyectamos HttpClient
+
+  get clientsUpdated$(): Observable<void> {
+    return this._clientsUpdated$.asObservable();
+  }
 
   // GET /api/client
   getClients(page: number, limit: number): Observable<PaginatedClientsResponse> {
@@ -59,12 +65,15 @@ constructor( private http: HttpClient) { } // inyectamos HttpClient
     );
   }
 
+  notifyClientUpdated(): void {
+    console.log('Servicio: Notificando clientes actualizados...');
+    this._clientsUpdated$.next();
+  }
+
   private handleError(error: any) {
     console.error('Servicio: Error al obtener clientes', error);
     return throwError(() => error('Algo salio mal, porfavor intentelo de nuevo mas tarde'));
   }
-
-  // TODO : Mejorar el manejo de errores
 
 }
     
